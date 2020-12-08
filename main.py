72aaf55b7848381feb630986f597403c41b1fe19
rohitgupta24 <rohit@allps.ch>
2020-12-08 15:54:03 +0530

    lips,eyes separated using predictor from dlib

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import dlib
 import cv2
+import numpy as np
 
 img = cv2.imread('1.jpg')
+
+
+def createBox(img, points, scale=5, show=False):
+    bbox = cv2.boundingRect(points)
+    x, y, w, h = bbox
+    imgCrop = img[y:y + h, x:x + w]
+    imgCrop = cv2.resize(imgCrop, (0, 0), None, scale, scale)
+    return imgCrop
 
 #shape predictor using save file
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
@@ -18,11 +27,23 @@ for face in faces:
 
     img = cv2.rectangle(img ,(x1,y1), (x2,y2),(0,255,0),(3))
     landmarks = predictor(img_grey,face)
+
+    myPoints =[]
+
     for n in range(68):
         x = landmarks.part(n).x
         y = landmarks.part(n).y
-        cv2.circle(img,(x,y),5,(39,78,255),cv2.FILLED)
 
+        myPoints.append([x, y])
+        
+        cv2.circle(img,(x,y),5,(39,78,255),cv2.FILLED)
+        # cv2.putText(img,str(n),(x,y-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,0,255),1) 
+
+    myPoints = np.array(myPoints)
+    imgLeftEye = createBox(img, myPoints[36:42])
+    imgLips = createBox(img, myPoints[48:61])
+    cv2.imshow('Lips', imgLips)
+    cv2.imshow('Left Eye', imgLeftEye)
 
 cv2.imshow("original", img)
 cv2.waitKey(0)
